a04e368947ffa5c2c679c149e86ddf423a042822
Teddy Wing <git@teddywing.com>
2022-03-07 22:52:17 +0100

    Try reading and parsing a YAML file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rusqlite;
+use yaml_rust;
+
 fn main() {
     println!("Hello, world!");
+
+    let text_data = std::fs::read_to_string("test.yml").unwrap();
+
+    {
+        use yaml_rust::YamlLoader;
+
+        let yaml_data = YamlLoader::load_from_str(&text_data).unwrap();
+
+        dbg!(yaml_data);
+    }
 }
