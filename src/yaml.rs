f4d5d7ee9897ed59cf83313774d160c832c091f3
Teddy Wing <git@teddywing.com>
2022-03-12 21:50:13 +0100

    Move `yaqlite::yaml` to `yaqlite::yaml::sql`
    Want to use `yaqlite::yaml` for the YAML extraction function.

--- src/yaml.rs
+++ src/yaml.rs
@@ -1,24 +1,3 @@
-use yaml_rust::yaml;
+mod sql;
 
-
-pub struct Yaml<'a>(pub &'a yaml::Yaml);
-
-impl<'a> rusqlite::ToSql for Yaml<'a> {
-    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
-        use rusqlite::types::ToSqlOutput;
-
-        let sql_output = match self.0 {
-            yaml::Yaml::Real(_) => ToSqlOutput::from(self.0.as_f64().unwrap()),
-            yaml::Yaml::Integer(_) => ToSqlOutput::from(self.0.as_i64().unwrap()),
-            yaml::Yaml::String(_) => ToSqlOutput::from(self.0.as_str().unwrap()),
-            yaml::Yaml::Boolean(_) => ToSqlOutput::from(self.0.as_bool().unwrap()),
-            yaml::Yaml::Array(_) => ToSqlOutput::from(rusqlite::types::Null),
-            yaml::Yaml::Hash(_) => ToSqlOutput::from(rusqlite::types::Null),
-            yaml::Yaml::Alias(_) => ToSqlOutput::from(rusqlite::types::Null),
-            yaml::Yaml::Null => ToSqlOutput::from(rusqlite::types::Null),
-            yaml::Yaml::BadValue => ToSqlOutput::from(rusqlite::types::Null),
-        };
-
-        Ok(sql_output)
-    }
-}
+pub use sql::*;
