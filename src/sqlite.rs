4ee0b82e55935c3a52e3ae0a11cd8959204e9b09
Teddy Wing <git@teddywing.com>
2022-03-21 01:57:09 +0100

    sqlite: Remove unused `affinity()` function
    Turns out I didn't need this. The `ToSql` and `FromSql` type conversion
    traits were what I needed instead to convert between YAML and SQLite
    types.

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -3,40 +3,6 @@ use rusqlite;
 use std::collections::HashMap;
 
 
-// TODO: Delete
-/// Get the fundamental SQLite datatype for a given type name.
-///
-/// Use the SQLite rules for type affinity described in:
-/// https://sqlite.org/datatype3.html#determination_of_column_affinity
-pub fn affinity(type_name: &str) -> rusqlite::types::Type {
-    use rusqlite::types::Type;
-
-    let type_name = type_name.to_uppercase();
-
-    if type_name.contains("INT") {
-        return Type::Integer;
-    } else if type_name.contains("CHAR")
-        || type_name.contains("CLOB")
-        || type_name.contains("TEXT")
-    {
-        return Type::Text;
-    } else if type_name.contains("BLOB")
-        || type_name.is_empty()
-    {
-        return Type::Blob;
-    } else if type_name.contains("REAL")
-        || type_name.contains("FLOA")
-        || type_name.contains("DOUB")
-    {
-        return Type::Real;
-    }
-
-    // TODO: Numeric affinity
-
-    Type::Text
-}
-
-
 #[derive(Debug)]
 pub struct Zero;
 
