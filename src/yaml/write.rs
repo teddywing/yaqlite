a4ff3950de526c6a6325c47ce334acf9576028ef
Teddy Wing <git@teddywing.com>
2022-03-27 05:58:36 +0200

    Add license (GNU GPLv3+)

--- src/yaml/write.rs
+++ src/yaml/write.rs
@@ -1,3 +1,20 @@
+// Copyright (c) 2022  Teddy Wing
+//
+// This file is part of Yaqlite.
+//
+// Yaqlite is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Yaqlite is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Yaqlite. If not, see <https://www.gnu.org/licenses/>.
+
 /// Adapt a `std::io::Write` type into a `std::fmt::Write`.
 pub struct IoAdapter<'a, T: std::io::Write> {
     inner: &'a mut T,
